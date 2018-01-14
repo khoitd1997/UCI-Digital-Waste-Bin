8599c14052cc34b020b9c734f0e2e9ddbc0a42a8
jpeezzy <justidl3@uci.edu>
2018-01-13 20:57:34 -0800

    added function to read json file

--- v.01/rotations.js
+++ v.01/rotations.js
@@ -1,7 +1,51 @@
-		var myIndex = 0;
-		carousel();
+var myIndex = 0;
+carousel();
+runProgram();
+function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function carousel() {
+async function runProgram()
+{
+	var objDate = new Date();
+	var sec1 = objDate.getSeconds();
+	var sec2 = sec1;
+	var objDate2;
+	while(true)
+	{	
+		objDate2 = new Date();
+		sec2 = objDate2.getMilliseconds();
+		await sleep(200); //sleep for 100 ms
+		readTextFile("file:///home/masa/gitProjects/UCI-Digital-Waste-Bin/v.01/test.json")
+		//console.log("asfeaesfa");
+		//console.log(sec2-sec1);
+		//if(sec2 - sec1 >= 400)
+		//	{
+		//	carousel(); 
+		//	}
+	}
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                //alert(allText);
+                //console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+async function carousel() {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
 	for (i = 0; i < x.length; i++) {
@@ -13,5 +57,13 @@ function carousel() {
 	var d = new Date();
 	var n = d.getSeconds();
 	var n2 = n;
+
 	setTimeout(carousel, 8000); // Change image every 2 seconds
 }
+
+    //var reader; //GLOBAL File Reader object for demo purpose only
+
+    /**
+     * Check for the various File API support.
+     */
+

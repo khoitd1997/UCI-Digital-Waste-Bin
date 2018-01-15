b4eca9e85889d589273a372b493199e968c45619
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-14 22:10:00 -0800

    added auto start of html and scale code on startup

--- v.01/setup_maintain.sh
+++ v.01/setup_maintain.sh
@@ -15,22 +15,18 @@ REBOOT_TIME="24:00"
 set -e 
 
 #MAINTAINANCE CODE
-if [ -e ${HOME}/${USER}/dwb_installed ]
+if [ -e ~/dwb_installed ]
 then
-chromium-browser index.html --allow-file-access-from-files
+sudo ufw enable
 sudo ifconfig wlan0 up
 sudo service ntp restart
-sudo ufw enable
+cd ~/UCI-Digital-Waste-Bin/
+git pull
 sudo apt-get update
 sudo apt-get dist-upgrade -y 
 sudo timedatectl set-timezone US/Pacific
 sudo ifconfig wlan0 down
 shutdown -r ${REBOOT_TIME}
-
-#launch the scale and html code
-chromium-browser ${HOME}/${USER}/{MAIN_PY_FILE_NAME}/index.html --allow-file-access-from-files --noerrdialogs --kiosk
-
-python3 scale_serial.py
 
 #INITIAL SETUP CODE
 else
@@ -60,12 +56,23 @@ echo "display_rotate=3" | sudo tee --append /boot/config.txt
 #enable auto-login
 sed -i -e '/autologin-user/s/#//' -e '/autologin-user/s/$/pi' /etc/lightdm/lightdm.conf
 
+#Add to startup file to run python script and the html code at boot as well 
+#as disable sleep mode and screensaver for rpi
+sudo sed -i -e '/@xscreensaver/s/^/#/' ~/.config/lxsession/LXDE-pi/autostart
+echo "@xset s off" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@xset -dpms" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@xset s noblank" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@sed -i 's/\"exited_cleanly\": true/' ~/.config/chromium/Default/Preferences " | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@sudo /usr/bin/python3 ~/UCI-Digital-Waste-Bin/v.01/scale_serial.py" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@point-rpi" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+echo "@chromium-browser --noerrdialogs --kiosk --incognito --allow-file-access-from-files ~/UCI-Digital-Waste-Bin/v.01/index.html" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
+
 #Create symlink for the scale, the number seems to be same for every scale
 echo "ACTION==\"add\",SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"SCALE\"" >> /etc/udev/rules.d/99-com.rules
 
 echo "Setup done, the system will reboot in 5 seconds"
 sleep 5
 
-#touch ${HOME}/dwb_installed
+#touch ~/dwb_installed
 sudo reboot
 fi

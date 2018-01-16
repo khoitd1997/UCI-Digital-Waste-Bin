2b180155b66357ba0d8ef1abafd9b0c5fd8752ee
abeljim <abelj1@uci.edu>
2018-01-15 21:12:24 -0800

    performance

--- final/compost/rotations.js
+++ final/compost/rotations.js
@@ -251,7 +251,7 @@ async function runProgram()
 	{	
 		objDate2 = new Date();
 		sec2 = objDate2.getMilliseconds();
-		await sleep(500); //sleep for 100 ms
+		await sleep(1000); //sleep for 100 ms
 		readTextFile("file:///home/pi/UCI-Digital-Waste-Bin/final/compost/result.json");
 		console.log(result1);
 		if(result2 != result1)

4ffe15bfbeef4b0fe25660ab36a25d0c8ad1aeef
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-15 19:49:39 -0800

    disable auto login and give sudo permission modifying udev

--- v.01/setup_maintain.sh
+++ v.01/setup_maintain.sh
@@ -43,7 +43,7 @@ sudo passwd -l root
 #update and install the necessary software
 sudo apt-get update
 sudo apt-get dist-upgrade -y 
-sudo apt-get install ${SOFTWARE}
+sudo apt-get install ${SOFTWARE} -y
 python3 -m pip install pyserial
 
 #enable firewall
@@ -59,7 +59,7 @@ echo "display_rotate=3" | sudo tee --append /boot/config.txt
 
 
 #enable auto-login
-sed -i -e '/autologin-user/s/#//' -e '/autologin-user/s/$/pi' /etc/lightdm/lightdm.conf
+# sudo sed -i -e '/autologin-user/s/#//' -e '/autologin-user/s/$/pi/' /etc/lightdm/lightdm.conf
 
 #Add to startup file to run python script and the html code at boot
 echo "@chromium-browser --noerrdialogs --kiosk --incognito --allow-file-access-from-files ~/UCI-Digital-Waste-Bin/v.01/index.html" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
@@ -77,7 +77,7 @@ echo "@point-rpi" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
 echo "@sudo ./~/UCI-Digital-Waste-Bin/v.01/setup_maintain.sh" | sudo tee --append ~/.config/lxsession/LXDE-pi/autostart
 
 #Create symlink for the scale, the number seems to be same for every scale
-echo "ACTION==\"add\",SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"SCALE\"" >> /etc/udev/rules.d/99-com.rules
+echo "ACTION==\"add\",SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"SCALE\"" | sudo tee --append /etc/udev/rules.d/99-com.rules
 
 echo "Setup done, the system will reboot in 5 seconds"
 sleep 5
